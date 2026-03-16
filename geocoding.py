1a3e99ff3b70f5e21c123964762cfe7204b50e71
ArthurDouilly <arthurdouilly@hotmail.fr>
2026-03-16 15:48:24 +0100

    [update] chapitre 1 complété

--- geocoding.py
+++ geocoding.py
@@ -17,7 +17,7 @@ json_files = list(DIR.glob("*.json"))
 
 # --- Initialisation du géocodeur
 geocoder = Nominatim(user_agent="tnah-équipe-2")
-limited_geocoder = RateLimiter(geocoder.geocode, min_delay_seconds=1)
+limited_geocoder = RateLimiter(geocoder.geocode, min_delay_seconds=5)
 
 
 # --- Fonctions
@@ -31,20 +31,26 @@ def read_json_file(file_path: Path) -> dict:
 def build_geocoding_query(data: dict, level: str) -> str:
     """Construit une requête de géocodage en fonction du niveau de précision demandé."""
 
-    if data.get("entites_geographiques"): # voir s'il y a bien "entités géographiques"
-        data = data.get("entites_geographiques") # si c'est le cas, le récupérer
-        if isinstance(data, list): # vérifier si c'est une liste
-            data = dict(zip( # si c'est une liste, on crée un dictionnaire en associant la liste a et b
-                ["toponyme","adresse", "voie", "ville", "pays"], # liste a, les niveaux
-                [item.get("entite") if isinstance(item, dict) else item for item in data] # liste b, les items contenus dans le json
-            ))
+# fix du prof avant correction des .json
+
+#   if data.get("entites_geographiques"): # voir s'il y a bien "entités géographiques"
+#        data = data.get("entites_geographiques") # si c'est le cas, le récupérer
+#        if isinstance(data, list): # vérifier si c'est une liste
+#            data = dict(zip( # si c'est une liste, on crée un dictionnaire en associant la liste a et b
+#                ["toponyme","adresse", "voie", "ville", "pays"], # liste a, les niveaux
+#                [item.get("entite") if isinstance(item, dict) else item for item in data] # liste b, les items contenus dans le json
+#            ))
 
     values = {
             "pays": data.get("pays") or "France",
             "ville": data.get("ville") or "Paris",
             "voie": data.get("voie"),
             "adresse": data.get("adresse"),
-            "toponyme" : data.get("toponyme"),
+            "toponyme": data.get("toponyme") # rajouts pour corriger les errances de Mistral
+            or data.get("édifice")
+            or data.get("place")
+            or data.get("établissement")
+            or ""
         }
 
     if level not in values:
@@ -81,6 +87,16 @@ def geocode(query: str):
     location = limited_geocoder(query)
     return location
 
+def create_geojson_feature(location: Location, properties: dict) -> geojson.Feature:
+    point = geojson.Point((location.longitude, location.latitude))
+    feature = geojson.Feature(geometry=point, properties=properties)
+    return feature
+
+def save_geocoding(feature: geojson.Feature, input_json_file: Path) -> None:
+    output_file = input_json_file.with_suffix(".geojson")
+    with open(output_file, "w") as file:
+        geojson.dump(feature, file, indent=2, ensure_ascii=False)
+    print(f"📁 Saved: {output_file}")
 
 # -- Traitement principal
 
@@ -101,6 +117,8 @@ def main():
     #         Sinon:
     #             Afficher "[{level}] ❌"
 
+    features = [] # liste originale
+
     for json_file in json_files: # première boucle récupérant les fichiers json un par un
         print(f"📁 Processing: {json_file.name}") # print indiquant sur quel fichier on opère
         data = read_json_file(json_file) # appel de la fonction 'read_json_file' pour lire le contenu du fichier
@@ -111,9 +129,27 @@ def main():
                 location = geocode(query)
                 if location is not None:
                     print(f"[{level}] ✅ : {location}") # indique le succès
+                    properties = { # création d'un dictionnaire à partir des informations de la boucle
+                        "extraction": data,
+                        "query": query,
+                        "level": level,
+                        "response": location.raw,
+                        "photo_id": json_file.stem,
+                    }
+                    feature = create_geojson_feature(location, properties) # création de la variable feature
+                    save_geocoding(feature, json_file) # création du .geojson
+                    features.append(feature) # ajout de la feature à la liste features
                     break # arrête la boucle
                 else : # s'il n'y a pas de résultats, indique l'échec
                     print(f"[{level}] ❌")
+
+    # le code ci-dessous crée un fichier débug additionnant toutes les features
+
+    debug_geojson_file = Path("debug_geocoding_results.geojson")
+    feature_collection = geojson.FeatureCollection(features)
+    with open(debug_geojson_file, "w") as file:
+        geojson.dump(feature_collection, file, indent=2, ensure_ascii=False)
+    print(f"📁 Saved on disk: {debug_geojson_file}")
 
 if __name__ == "__main__":
     main()
